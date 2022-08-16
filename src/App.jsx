3cee82864d25d5508bf3be582c32de169297a83e
Joao Vitor Freitas <jvfreitas02@gmail.com>
2022-08-16 14:37:29 -0300

    Implementação Bootstrap, consumo Wheater-API renderizar dados em tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [city, setCity] = useState('');
+  const [weatherForecast, setweatherForecast] = useState(null);
+  function handleChange(e) {
+    setCity(e.target.value);
+    console.log(city);
+  }
 
+  function handleSearch() {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=f745a7c9c3114847874161805221608&q=${city}&lang=pt`
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setweatherForecast(data);
+      });
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand">j0tta Weather</a>
+      </nav>
 
-export default App
+      <main className="container">
+        <div className="jumbotron">
+          <h1>Verifique agora a previsão do tempo d asua cidade!</h1>
+          <p className="lead">
+            Digite o nome da sua cidade no campo abaixo e em seguida clique em
+            pesquisar
+          </p>
+
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                onChange={handleChange}
+                value={city}
+              />
+            </div>
+          </div>
+
+          <button className="btn btn-primary btn-lg" onClick={handleSearch}>
+            Pesquisar
+          </button>
+
+          {weatherForecast ? (
+            <div>
+              <div className="mt-4 d-flex align-items-center">
+                <div>
+                  <img src={weatherForecast.current.condition.icon} />
+                </div>
+                <div>
+                  <h3>
+                    Hoje o dia está: {weatherForecast.current.condition.text}
+                  </h3>
+                  <p className="lead">
+                    Temperatura: {weatherForecast.current.temp_c}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </main>
+    </div>
+  );
+}
